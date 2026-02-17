9efdad2b6471edfc38cb443bd64851da972c104c
Roman <romazan.26@gmail.com>
2026-02-17 12:48:42 +0300

    Initial Commit

---
+++ BellsBalance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BellsBalance
+//
+//  Created by Роман Главацкий on 17.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
